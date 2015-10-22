8861f9d331c14b05e3f31dc1516675f2924498a0
Peter DeMartini <thepeterdemartini@gmail.com>
2015-10-22 11:06:26 -0700

    upgraded meshblu and used common library for getting the path

--- meshblu.js
+++ meshblu.js
@@ -1,251 +1,199 @@
-var skynet = require('meshblu');
+var meshblu = require('meshblu');
 var _ = require('lodash');
 
-var rest = require('rest');
-var mime = require('rest/interceptor/mime');
-var client = rest.wrap(mime);
+var when = require('when');
+
+var getDevices = require('./lib/getDevices');
+var register = require('./lib/register');
+var claim = require('./lib/claim');
+
 var RPC_TIMEOUT = 10000;
 
 function isBroadcast(data){
-    var broadcast = false;
-    _.forEach(data.devices, function(device){
-        if(device === '*' || device === 'all'){
-            broadcast = true;
-        }
-    });
-    return broadcast;
+  var broadcast = false;
+  _.forEach(data.devices, function(device){
+    if(device === '*' || device === 'all'){
+      broadcast = true;
+    }
+  });
+  return broadcast;
 }
 
-module.exports = function(RED) {
+function init(RED) {
 
-    function meshbluserverNode(n) {
-        RED.nodes.createNode(this,n);
-        this.server = n.server;
-        this.port = n.port;
-        this.uuid = n.uuid;
-        this.token = n.token;
+  function meshbluserverNode(n) {
+    var self = this;
+    RED.nodes.createNode(self,n);
+    self.server = n.server || 'meshblu.octoblu.com';
+    self.port = n.port || 80;
+    self.uuid = n.uuid;
+    self.token = n.token;
 
-        var options = {
-            server: this.server,
-            port: this.port,
-            uuid: this.uuid,
-            token: this.token
-        };
-        this.conn = skynet.createConnection(options);
+    var options = {
+      server: self.server,
+      port: self.port,
+      uuid: self.uuid,
+      token: self.token
+    };
+    self.conn = meshblu.createConnection(options);
 
-        var self = this;
-        self.directs = [];
-        self.subs = [];
+    self.directs = [];
+    self.subs = [];
 
-        self.conn.on('ready', function(data){
-            self.conn.connected = true;
+    self.conn.on('ready', function(data){
+      self.conn.connected = true;
 
-            _.forEach(RED.nodes.getFlows(), function(n){
-                if(n.server === self.id){
-                    var mnode = RED.nodes.getNode(n.id);
-                    if(n.type === 'meshblu in'){
-                        if(mnode.directToMe){
-                            self.directs.push(mnode);
-                        }else{
-                            self.subs.push(mnode);
-                            self.conn.subscribe({ uuid: mnode.uuid }, function(err){
-                                console.log('subscribed to', mnode.uuid, err);
-                            });
-                        }
-                    }
+      _.forEach(RED.nodes.getFlows(), function(n){
+        if(n.server === self.id){
+          var mnode = RED.nodes.getNode(n.id);
+          if(n.type === 'meshblu in'){
+            if(mnode.directToMe){
+              self.directs.push(mnode);
+            }else{
+              self.subs.push(mnode);
+              self.conn.subscribe({ uuid: mnode.uuid }, function(err){
+                console.log('subscribed to', mnode.uuid, err);
+              });
+            }
+          }
 
-                    if(n.type === 'meshblu in' || n.type === 'meshblu out'){
-                        mnode.status({fill:"green",shape:"dot",text:"connected"});
-                    }
+          if(n.type === 'meshblu in' || n.type === 'meshblu out'){
+            mnode.status({fill:"green",shape:"dot",text:"connected"});
+          }
+        }
+      });
+
+    });
+
+    self.conn.on('message', function(data, fn){
+      if(data.devices){
+        if(!_.isArray(data.devices)){
+          data.devices = [data.devices];
+        }
+        if(isBroadcast(data)){
+          _.forEach(self.subs, function(sub){
+            if(sub.uuid === data.fromUuid){
+              sub.send(data);
+            }
+          });
+        }else{
+          _.forEach(self.directs, function(direct){
+            direct.send(data);
+          });
+        }
+      }
+    });
+
+    self.conn.on('error', function(err){
+       console.log('error in meshblu connection', err);
+    });
+
+    self.on('close', function() {
+      if (self.conn.connected) {
+        console.log('close meshblu connection');
+        self.conn.close();
+      }
+    });
+
+    self.conn.alternateMethod = function(name, msg, fn){
+      try{
+        self.conn[name](msg, fn);
+      }catch(exp){
+        console.log('error calling alternateMethod', name, msg, exp);
+      }
+    }
+  }
+  RED.nodes.registerType("meshblu-server", meshbluserverNode);
+
+  function meshbluInNode(n) {
+    var self = this;
+    RED.nodes.createNode(self,n);
+    self.topic = n.topic;
+    self.server = n.server;
+    self.serverConfig = RED.nodes.getNode(self.server);
+    self.directToMe = n.directToMe;
+    self.uuid = n.uuid;
+  }
+  RED.nodes.registerType("meshblu in",meshbluInNode);
+
+  function meshbluOutNode(n) {
+    var self = this;
+    RED.nodes.createNode(self,n);
+    self.server = n.server;
+    self.serverConfig = RED.nodes.getNode(self.server);
+    self.broadcast = n.broadcast;
+    self.uuid = n.uuid;
+    self.forwards = n.forwards;
+
+    if (self.serverConfig) {
+      var node = self;
+      self.on("input",function(msg) {
+        if(!msg.devices){
+          if(self.broadcast){
+            msg.devices = ['*'];
+          }else{
+            if(self.uuid){
+              msg.devices = [self.uuid];
+            }
+          }
+        }
+        if(msg.devices){
+          try{
+            var conn = self.serverConfig.conn;
+            if(self.forwards && !msg.reply){
+              var responded = false;
+              function handleResponse(resp){
+                if(!responded){
+                  responded = true;
+                  msg.payload = resp;
+                  node.send(msg);
                 }
-            });
-
-        });
-
-        self.conn.on('message', function(data, fn){
-            if(data.devices){
-                if(!Array.isArray(data.devices)){
-                    data.devices = [data.devices];
+              }
+              if(msg.alternateMethod){
+                conn.alternateMethod(msg.alternateMethod, msg, handleResponse);
+              }else{
+                conn.message(msg, handleResponse);
+              }
+              setTimeout(function(){
+                if(!responded){
+                  responded = true;
+                  msg.payload = {error: "request timeout"};
+                  self.send(msg);
                 }
-                if(isBroadcast(data)){
-                    _.forEach(self.subs, function(sub){
-                        if(sub.uuid === data.fromUuid){
-                            sub.send(data);
-                        }
-                    });
+              },msg.timeout || RPC_TIMEOUT);
+            }else{
+              if(msg.reply){
+                if(msg.ack && msg.fromUuid){
+                  msg.devices = msg.fromUuid;
+                  delete msg.fromUuid;
+                  msg.payload = msg.reply;
+                  delete msg.reply;
+                  conn.alternateMethod('_messageAck', msg);
                 }else{
-                    _.forEach(self.directs, function(direct){
-                        direct.send(data);
-                    });
+                  console.log('cant reply without an ack and fromUuid', msg);
                 }
+              }else{
+                conn.message(msg);
+              }
 
             }
-        });
-
-        self.conn.on('error', function(err){
-           console.log('error in meshblu connection', err);
-        });
-
-        self.on('close', function() {
-            if (self.conn.connected) {
-                console.log('close meshblu connection');
-                self.conn.close();
-            }
-        });
-
-        self.conn.alternateMethod = function(name, msg, fn){
-            try{
-                self.conn[name](msg, fn);
-            }catch(exp){
-                console.log('error calling alternateMethod', name, msg, exp);
-            }
+          }catch(err){
+            console.log('errr sending', err);
+          }
         }
+
+      });
+    } else {
+      self.error("missing server configuration");
     }
-    RED.nodes.registerType("meshblu-server", meshbluserverNode);
+  }
 
-    function meshbluInNode(n) {
-        RED.nodes.createNode(this,n);
-        this.topic = n.topic;
-        this.server = n.server;
-        this.serverConfig = RED.nodes.getNode(this.server);
-        this.directToMe = n.directToMe;
-        this.uuid = n.uuid;
-    }
-    RED.nodes.registerType("meshblu in",meshbluInNode);
+  RED.nodes.registerType("meshblu out",meshbluOutNode);
 
-    function meshbluOutNode(n) {
-        RED.nodes.createNode(this,n);
-        this.server = n.server;
-        this.serverConfig = RED.nodes.getNode(this.server);
-        this.broadcast = n.broadcast;
-        this.uuid = n.uuid;
-        this.forwards = n.forwards;
-        var self = this;
-
-        if (this.serverConfig) {
-            var node = this;
-            this.on("input",function(msg) {
-                if(!msg.devices){
-                    if(self.broadcast){
-                        msg.devices = ['*'];
-                    }else{
-                        if(self.uuid){
-                            msg.devices = [self.uuid];
-                        }
-                    }
-                }
-                if(msg.devices){
-                    try{
-                        var conn = self.serverConfig.conn;
-                        if(self.forwards && !msg.reply){
-                          var responded = false;
-                          function handleResponse(resp){
-                            if(!responded){
-                                responded = true;
-                                msg.payload = resp;
-                                node.send(msg);
-                            }
-                          }
-                          if(msg.alternateMethod){
-                            conn.alternateMethod(msg.alternateMethod, msg, handleResponse);
-                          }else{
-                            conn.message(msg, handleResponse);
-                          }
-                          setTimeout(function(){
-                            if(!responded){
-                              responded = true;
-                              msg.payload = {error: "request timeout"};
-                              self.send(msg);
-                            }
-                          },msg.timeout || RPC_TIMEOUT);
-                        }else{
-                          if(msg.reply){
-                            if(msg.ack && msg.fromUuid){
-                              msg.devices = msg.fromUuid;
-                              delete msg.fromUuid;
-                              msg.payload = msg.reply;
-                              delete msg.reply;
-                              conn.alternateMethod('_messageAck', msg);
-                            }else{
-                              console.log('cant reply without an ack and fromUuid', msg);
-                            }
-                          }else{
-                            conn.message(msg);
-                          }
-
-                        }
-                    }catch(err){
-                        console.log('errr sending', err);
-                    }
-                }
-
-            });
-        } else {
-            this.error("missing server configuration");
-        }
-    }
-
-    RED.httpAdmin.get("/meshblu/register", function(req,res) {
-        try{
-
-            var server = req.query.server.toLowerCase();
-            if(server.indexOf('http') !== 0){
-                server = 'http://' + server;
-            }
-            var path = server+':'+req.query.port+'/devices';
-
-            client({
-                path: path,
-                method: 'POST',
-                params: {type: 'nodered', name: 'nodered'}
-            })
-            .then(function(data){
-                console.log('resp ok', data.entity);
-                res.send(data.entity);
-            }, function(err){
-                console.log('error', err);
-                res.send(500);
-            });
-
-        }catch(exp){
-            console.log('err', exp);
-            res.send(500);
-        }
-
-    });
-
-    RED.httpAdmin.get("/meshblu/local", function(req,res) {
-        try{
-
-            var server = req.query.server.toLowerCase();
-            if(server.indexOf('http') !== 0){
-                server = 'http://' + server;
-            }
-            var path = server+':'+req.query.port+'/localdevices';
-
-            client({
-                path: path,
-                method: 'GET',
-                headers: {
-                    skynet_auth_uuid: req.query.uuid,
-                    skynet_auth_token: req.query.token,
-                }
-            })
-            .then(function(data){
-                //console.log('resp ok', data.entity);
-                res.send(data.entity);
-            }, function(err){
-                console.log('error', err);
-                res.send(500);
-            });
-
-        }catch(exp){
-            console.log('err', exp);
-            res.send(500);
-        }
-
-    });
-
-
-    RED.nodes.registerType("meshblu out",meshbluOutNode);
+  //routes
+  RED.httpAdmin.get("/meshblu/register", register);
+  RED.httpAdmin.get("/meshblu/getDevices", getDevices);
+  RED.httpAdmin.get("/meshblu/claim", claim);
 }
+
+module.exports = init;
